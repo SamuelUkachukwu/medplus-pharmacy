622ef141086a18092f23f082c3c60460216a0202
SamuelUkachukwu <shybold@yahoo.com>
2022-03-17 23:36:38 +0000

    Add patient_drug_history functionality

--- run.py
+++ run.py
@@ -1,6 +1,5 @@
 import gspread
 from google.oauth2.service_account import Credentials
-import random
 from pprint import pprint
 
 
@@ -29,10 +28,12 @@ def collect_user_input():
             if str(f"MP{patient_id}") in patients_list:
                 print('Patient account found')
                 print("Retrieving Patient's Drug History...")
-                patient_drug_history()
+                patient_drug_history(patient_id)
             else:
-                print(f"An account could not be found for the provided Patient ID: {patient_id}")
-                new_patient = input("Do You want to create a new account? Y/N :").lower()
+                print(
+                    f"An account does not exist for the provided ID:{patient_id}"
+                    )
+                new_patient = input("Create a new account? Y/N :").lower()
                 if new_patient == 'y':
                     create_new_patient_account()
                 elif new_patient == 'n':
@@ -45,7 +46,7 @@ def collect_user_input():
 
 def validate_patient_id(values):
     """
-    converts all values to intergers. 
+    converts all values to intergers.
     Raises ValueError if string cannot be converted into intergers
     or the length of the value is not 3
     """
@@ -53,8 +54,8 @@ def validate_patient_id(values):
         [int(value) for value in values]
         if len(values) != 3:
             raise ValueError(
-            f"Last three digits is required, you entered {len(values)}"
-            )
+                f"Last three digits is required, you entered {len(values)}"
+                )
     except ValueError as error:
         print(f"invalid patient Id {error}, please try again")
         return False
@@ -66,23 +67,21 @@ def create_new_patient_account():
     function to create new patient account
     """
     new_patient = []
-    new_sheet_header = ['Date', 'Medication', 'Brand Name', 'Prescribed for', 'Dosage', 'Dosing Frequency',	'Special Notes']
+    new_sheet_header = [
+        'Date', 'Medication', 'Brand Name',
+        'Prescribed for', 'Dosage',
+        'Dosing Frequency', 'Special Notes'
+        ]
     new_patient_id = create_new_patient_id()
     new_patient.append(new_patient_id)
-    fname = input("Enter Your First Name\n")
+    fname = input("Enter Your First Name\n").capitalize()
     new_patient.append(fname)
-    lname = input("Enter Your Last Name:\n")
+    lname = input("Enter Your Last Name:\n").capitalize()
     new_patient.append(lname)
     SHEET.worksheet('patients').append_row(new_patient)
-    print(new_patient)
     SHEET.add_worksheet(title=f"{new_patient_id}", rows=100, cols=20)
     SHEET.worksheet(f"{new_patient_id}").append_row(new_sheet_header)
-
-# worksheet = SHEET.add_worksheet(title="007", rows=100, cols=20)
-
-#     patients_list = SHEET.worksheet('patients').col_values(1)
-#     new_patient = list(patients_list[-1][-3:])
-#     print(new_patient[2])
+    patient_drug_history()
 
 
 def create_new_patient_id():
@@ -100,32 +99,16 @@ def create_new_patient_id():
         break
 
 
-# create_new_patient_id()
-# patients_list = SHEET.worksheet('patients').col_values(1)
-# patients_list.append('MP004')
-# print((patients_list))
-# new = [1,2,2,2,3,4]
-# SHEET.worksheet('patients').append_row(patients_list)
-# SHEET.worksheet('patients').append_row(new)
-
-# print(new)
-def patient_drug_history():
+def patient_drug_history(patient_id):
     """
     patient drug history data retrived from worksheet
     """
-    print('this is the drug history')
-
+    print('Patient Drug History Retrieved\n')
+    patients_list = SHEET.worksheet('patients').col_values(1)
+    index = patients_list.index(f"MP{patient_id}")
+    patient_details = SHEET.worksheet("patients").row_values(int(index)+1)
+    print(f" Name: {patient_details[1]} {patient_details[2]}\n Patient ID: {patient_details[0]}")
+    
 
 print('Welcome To Medplus Pharmacy "Your health, Our care"\n')
 collect_user_input()
-
-# class Patient():
-#     """
-#     Create an instance of patient
-#     """
-#     def __init__(self, first_name, last_name, id_num):
-#         self.first_name = first_name
-#         self.last_name = last_name
-#         self.id_num = f"MP{id_num}"
-#     def description(self):
-#         print(f" First Name: {self.first_name}\n Surname: {self.last_name}\n ID:{self.id_num}")
