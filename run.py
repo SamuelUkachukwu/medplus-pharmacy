17c8866785865d327d33f417964c1cb745f8cb1d
SamuelUkachukwu <shybold@yahoo.com>
2022-03-18 16:54:30 +0000

    updated patient_drug_history function to display last recorded drug history. Added enter_drug_history function

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import gspread
 from google.oauth2.service_account import Credentials
-from pprint import pprint
+# from pprint import pprint
 
 
 SCOPE = [
@@ -18,6 +18,8 @@ SHEET = GSPREAD_CLIENT.open('medplus_pharmacy')
 def collect_user_input():
     """
     Captures user input of Patients ID
+    request input validation
+    Calls for new account creation
     """
     while True:
         print(f"Please enter the last three digits of the Patient ID")
@@ -26,8 +28,7 @@ def collect_user_input():
         if validate_patient_id(patient_id):
             patients_list = SHEET.worksheet('patients').col_values(1)
             if str(f"MP{patient_id}") in patients_list:
-                print('Patient account found')
-                print("Retrieving Patient's Drug History...")
+                print('Patient account found\n')
                 patient_drug_history(f"MP{patient_id}")
             else:
                 print(f"An account does not exist for the provided ID:{patient_id}")
@@ -61,15 +62,18 @@ def validate_patient_id(values):
 
 
 def create_new_patient():
-    """.git/
+    """
+    Creates a new patient account by calling the listed functions
     """
     patient_id = create_new_patient_id()
+    print("\nCreating New Account…\n")
     create_new_patient_account(patient_id)
 
 
 def create_new_patient_id():
     """
-    function to create new patient id
+    generates new id
+    verify id generated is not already in the patient list
     """
     patients_list = SHEET.worksheet('patients').col_values(1)
     while True:
@@ -84,11 +88,12 @@ def create_new_patient_id():
 
 def create_new_patient_account(data):
     """
-    function to create new patient account
+    Create new patient account writes to worksheet 'patient'
+    Create new worksheet for new account created
     """
     new_patient = []
     new_sheet_header = [
-        'Date', 'Medication', 'Brand Name',
+        'Date', 'Medication',
         'Prescribed for', 'Dosage',
         'Dosing Frequency', 'Special Notes'
         ]
@@ -101,6 +106,7 @@ def create_new_patient_account(data):
     SHEET.worksheet('patients').append_row(new_patient)
     SHEET.add_worksheet(title=f"{new_patient_id}", rows=100, cols=20)
     SHEET.worksheet(f"{new_patient_id}").append_row(new_sheet_header)
+    print("\nNew Account Created\n")
     patient_drug_history(new_patient_id)
 
 
@@ -108,12 +114,46 @@ def patient_drug_history(data):
     """
     patient drug history data retrived from worksheet
     """
+    print("Retrieving Patient's Drug History...")
     print('Patient Drug History Retrieved\n')
     patients_list = SHEET.worksheet('patients').col_values(1)
     index = patients_list.index(data)
-    patient_details = SHEET.worksheet("patients").row_values(int(index)+1)
-    print(f" Name: {patient_details[1]} {patient_details[2]}\n Patient ID: {patient_details[0]}")
+    patient = SHEET.worksheet("patients").row_values(int(index)+1)
+    print(f" Name: {patient[1]} {patient[2]}\n Patient ID: {patient[0]}")
+    history = SHEET.worksheet(f"{patient[0]}").get_all_values()
+    # print(history[0])
+    # print(history[-1])
+    if history[0] == history[-1]:
+        print("\nPatient has no recorded history")
+        enter_drug_history()
+    else:
+        print(f"\nDate: {history[-1][0]}")
+        print(f"Medication: {history[-1][1]}")
+        print(f"Prescribed for: {history[-1][2]}")
+        print(f"Dosage: {history[-1][3]}")
+        print(f"Dosing frequency: {history[-1][4]}")
+        print(f"Notes: {history[-1][5]}\n")
+        enter_drug_history()
 
+
+def enter_drug_history():
+    """
+    accept user input and append it to worksheet
+    """
+    request = input("Enter new details? Y/N :")
+    print("Hello You Are Here Now.")
+    print(request)
+
+
+
+
+    # if new_patient == 'y':
+    #                 create_new_patient()
+    #             elif new_patient == 'n':
+    #                 collect_user_input()
+    #             else:
+    #                 print('Invalid Answer')
+    #                 collect_user_input()
 
 print('Welcome To Medplus Pharmacy "Your health, Our care"\n')
 collect_user_input()
